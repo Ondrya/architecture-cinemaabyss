b01efe83ab97283b7d86938f25c64fbae4022c5f
Евсюгин Андрей <andrey.evsugin@major-express.ru>
2025-10-26 15:19:53 +0300

    fix docker-compose

--- src/microservices/events/src/index.js
+++ src/microservices/events/src/index.js
@@ -1,7 +1,7 @@
 const { Kafka } = require('kafkajs');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8082;
 
 
 app.use(express.json());
